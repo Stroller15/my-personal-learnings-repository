ad4ef26a0540f566a7533978fbcea4ee663220be
Your Name <shubhamverma6351@gmail.com>
2023-02-01 23:20:15 +0530

    basic js

---
+++ JAVASCRIPT/day_1/basic.js
@@ -0,0 +1,21 @@
+//we use log for print value
+console.log("hello world");
+
+// we can use multiple arg in console.log
+
+console.log("hell" , "shubhma" , "verma");
+
+// So far, we saw how to display text using the console.log(). If we are printing text or string using console.log(), the text has to be inside the single quotes, double quotes, or a backtick. Example:
+ 
+console.log('hello babu musay');
+console.log("hello, world");
+console.log(`hello, world`);
+
+// we can do all airthmatic operation in it
+
+console.log( "addition" , 2 + 3);
+console.log("substraction", 3 - 2);
+console.log("multiplication", 2 * 3);
+console.log("division", 3 / 2);
+console.log("modulus", 3 % 2);
+console.log("exponentiation", 3 ** 2);
