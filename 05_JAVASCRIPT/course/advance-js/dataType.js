99af2b3b4fb0bc067450e9d72c7ab2da00e3ec88
Your Name <shubhamverma6351@gmail.com>
2023-03-04 13:51:33 +0530

    learn about datatype

---
+++ 05_JAVASCRIPT/course/advance-js/dataType.js
@@ -0,0 +1,49 @@
+//  Data Type
+
+// js is a loosely type language
+
+// let age = 25;
+
+// age = "two"
+
+// console.log(age);
+
+//  Primitive data type
+
+// ! 1. Number
+
+// let age = 34;
+// let temp = -34;
+// let price = 430.3;
+
+
+//! 2.String 
+
+//  let name = 'Rakesh';
+
+
+// ! 3.Boolean 
+// let isLoggedIn = true;
+// let hasPermission = false;
+
+// ! 4. undefiended
+
+// let age;
+// console.log(age);
+
+// ! BigInt
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// 9007199254740991
+
+
+
+
+
+
+
+
+
+ 
+
+
