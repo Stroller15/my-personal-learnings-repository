d75d6b0a1c05701a2717fdcd4a291abc1777a7e7
Your Name <shubhamverma6351@gmail.com>
2023-03-04 14:40:11 +0530

    operator

---
+++ 05_JAVASCRIPT/course/advance-js/operator.js
@@ -0,0 +1,14 @@
+/*
+1. Assignment operators
+2. Comparison operators
+3.  Arithmetic operators
+4. Bitwise operators
+5. Logical operators
+6. String operators
+7. Conditional(ternary) operator
+8. comma operator 
+9. Unary operators
+10. Relational operators
+*/
+
+
