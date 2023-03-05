e63a8f955c4f5462d954bbaa7b17b237b8d5044b
Your Name <shubhamverma6351@gmail.com>
2023-03-05 15:26:28 +0530

    async.js

---
+++ 05_JAVASCRIPT/course/advance-js/async.js
@@ -0,0 +1,13 @@
+// ! Asynchronous Javascript
+
+console.log('Hello');
+
+setTimeout(() => {
+    console.log("I am from SetTimeout");
+}, 3000)
+
+console.log('Javascript');
+
+// Event listener are async
+
+// In simple words we makes async javascript when we need any request in any delay call stack , queue, web api
