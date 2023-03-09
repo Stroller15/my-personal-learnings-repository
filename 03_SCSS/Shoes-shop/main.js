a4def41e7fafaf30f1899ff65b1de9ceee3c1231
Your Name <shubhamverma6351@gmail.com>
2023-03-09 14:16:58 +0530

    shoe-shop

---
+++ 03_SCSS/Shoes-shop/main.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
