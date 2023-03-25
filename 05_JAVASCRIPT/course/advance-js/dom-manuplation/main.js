bb97f5f28c3fc19869c55348dae34ba4707a0ab3
Your Name <shubhamverma6351@gmail.com>
2023-03-25 18:51:31 +0530

    dom manuplation

---
+++ 05_JAVASCRIPT/course/advance-js/dom-manuplation/main.js
@@ -0,0 +1,12 @@
+//? Dom manuplation Document object model
+
+// !  1. getElementByIddddddd
+
+// const heading = document.getElementById('heading');
+// console.log(heading);
+
+
+// ! 2. By tag name
+
+const heading = document.getElementsByTagName('h1')
+console.log(heading)
